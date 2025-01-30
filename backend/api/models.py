b8b1f398d0f76f889fbab9b8e3b836c8ab4af6a1
Gourav Kumar Dhiman <gkrdhiman@gmail.com>
2025-01-30 20:41:19 +0530

    feat: create initial API endpoints and database models
    - Add serializers for API data transformation
    - Set up database models and initial migration
    - Configure URL routing for API endpoints
    - Create management command for database population
    - Update project settings and gitignore

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,63 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to="products/", null=True, blank=True)
+    tags = models.JSONField(default=list , blank=True , null=True , help_text="List of tags")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        PENDING = "PENDING"
+        COMPLETED = "COMPLETED"
+        CANCELLED = "CANCELLED"
+
+    order_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField(
+        Product, through="OrderItem", related_name="orders"
+    )
+    status = models.CharField(
+        max_length=20, choices=OrderStatus.choices, default=OrderStatus.PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"Order {self.order.order_id} Item {self.product.name}"
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+@api_view(["GET"])
+def product_list(request):
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+@api_view(["GET"])
+def product_detail(reqquest, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
